3876ef71aa37877d0ab40e68bb2d2b63d9899631
OlegKushnir <kushnir.oleg87@gmail.com>
2023-06-19 17:50:58 +0300

    test

--- react-app/src/backend/backend.js
+++ react-app/src/backend/backend.js
@@ -2,13 +2,19 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:5000/api";
 
 export async function fetchTransactions() {
-    const res = await axios.get("transactions");
-    return res.data;
+  const res = await axios.get("transactions");
+  return res.data;
 }
 
 export async function fetchBalance(id) {
-    const res = await axios.get(`accounts/${id}`);
-    return res.data.balance;
+  const res = await axios.get(`accounts/${id}`);
+  return res.data.balance;
+}
+
+export async function fetchAccounts() {
+  const res = await axios.get("accounts");
+  return res.data;
+  // return [{account_id: "0c18647a-ba8a-419f-8d72-af7ee92429c0", balance: 120}]
 }
 
 export async function createTransaction(transaction) {
